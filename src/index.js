7665499114bf005fcbb6a59908ad151fd17cb51f
Cristian Vidal <cristiangvidal99@gmail.com>
2026-01-03 03:15:41 -0300

    change: cors options

--- src/index.js
+++ src/index.js
@@ -7,10 +7,24 @@ const routes = require('./routes/apiRoutes');
 const { PORT } = require('./configs/constants.js');
 const app = express();
 
-app.use(cors());
+// Configuración de CORS
+const corsOptions = {
+    origin: [
+        'https://notesfe-k7ii.onrender.com',
+        'http://localhost:3000',
+        'http://localhost:5173',
+        'http://localhost:8080'
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.use('/api', cors(), routes);
+app.use('/api', routes);
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en puerto ${PORT}`);
